e688e181548f6d013902e77fa7c6a37c42fae7e6
AdolfoAldana <aldanachigna@gmail.com>
2023-04-20 21:22:18 -0600

    Agregando carpetas

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <cConn.h>
+#include "C:/Program Files/MySQL/MySQL Server 8.0/include/mysql.h"
+#include "C:/Program Files/MySQL/MySQL Server 8.0/include/mysqld_error.h"
 using namespace std;
 int main(){
     cout<<"Hola mundo"<<endl;
