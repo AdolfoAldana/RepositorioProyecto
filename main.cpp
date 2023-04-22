937890e96e51303d2c7048108b5e398ce9e724ef
AdolfoAldana <aldanachigna@gmail.com>
2023-04-21 20:57:10 -0600

    Cambios Conexion

--- main.cpp
+++ main.cpp
@@ -1,16 +1,28 @@
 #include <iostream>
-#include ".\cConn.h"
+//#include ".\cConn.h"
 #include "C:\Program Files\MySQL\MySQL Server 8.0\include\mysql.h"
 #include "C:\Program Files\MySQL\MySQL Server 8.0\include\mysqld_error.h"
 
-
-
 using namespace std;
+//int main(){
+   //cConn connDB;
+   //MYSQL *objDatos;
+    //if(!(objDatos = mysql_init(0)))    {
+      //  cout<<"Error al cargar driver de la base de datos";
+        //return 0;
+   // }
+//}
 int main(){
-   cConn connDB;
    MYSQL *objDatos;
-    if(!(objDatos = mysql_init(0)))    {
-        cout<<"Error al cargar driver de la base de datos";
+   objDatos = mysql_init(0);
+   objDatos = mysql_real_connect(objDatos,"localhost","root","root","ferreteria",3306,NULL,0);
+    if(objDatos)    {
+        cout<<"Base de datos conectada";
         return 0;
     }
+    else{
+        cout<<"Error..";
+    }
+    system("pause");
+    return 0;
 }
